69c59fd313a8a1344fbe8aee7d48711e3210365e
maxion <maxion03@gmail.com>
2019-02-15 19:09:26 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import * as bodyParser from "body-parser";
+import * as express from "express";
+import { Request, Response } from "express";
+import * as http from 'http';
+
+const app:express.Application = express();
+
+const mockData = {
+    a:1, b:2
+}
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({status: "ok"});
+});
+
+app.get('/test', (req: Request, res: Response) => {
+    res.status(200).json(mockData);
+});
+
+
+let httpPort = 3000;
+app.set("port", httpPort);
+var httpServer = http.createServer(app);
+
+//listen on provided ports
+httpServer.listen(httpPort, (data) => {
+    console.log(`Listening on port ${httpPort}`)
+});
