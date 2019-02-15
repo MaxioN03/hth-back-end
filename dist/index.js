b16427fe2b0de3d782392471615f5d4b3aa2c1a7
maxion <maxion03@gmail.com>
2019-02-15 19:58:34 +0300

    Change port

--- dist/index.js
+++ dist/index.js
@@ -82,7 +82,7 @@ app.get('/', (req, res) => {
 app.get('/test', (req, res) => {
     res.status(200).json(mockData);
 });
-let httpPort = 3000;
+let httpPort = process.env.PORT || 9000;
 app.set("port", httpPort);
 let httpServer = http.createServer(app);
 //listen on provided ports
