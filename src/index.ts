6e455b7d4fd479e099d50770103dad7f0e77b4ac
maxion <maxion03@gmail.com>
2019-02-16 20:08:57 +0300

    Add headers

--- src/index.ts
+++ src/index.ts
@@ -22,10 +22,12 @@ db.once('open', () => {
 });
 
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.get('/', (req: Request, res: Response) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
     res.status(200).json({status: "ok"});
 });
 app.use('/user', userRouter);
