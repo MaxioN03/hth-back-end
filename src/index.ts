27c1579a310fc6aad76faf23c04354083f40721a
maxion <maxion03@gmail.com>
2019-02-16 11:22:12 +0300

    Connect database

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,40 @@ import * as bodyParser from "body-parser";
 import * as express from "express";
 import {Request, Response} from "express";
 import * as http from 'http';
+import UserDAO from "./DAO/models/User";
+
+console.log('UserDAO start',UserDAO);
 
 const app: express.Application = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+let mongoose = require('mongoose');
+let dbUrl = 'mongodb://maxion:maxionhth123@ds137605.mlab.com:37605/hth-db';
+mongoose.connect(dbUrl);
+
+let db = mongoose.connection;
+db.on('error', err => {
+    console.log('error', err)
+});
+db.once('open', () => {
+    console.log('MongoDB connected')
+
+    console.log(UserDAO.create({first_name: 'Yahor', last_name: 'Kutz'}));
+});
+
 const mockData = {
-    a: 1, b: 2
-}
+    a: 1, b: 2, c: 3
+};
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({status: "ok"});
 });
 
 app.get('/test', (req: Request, res: Response) => {
-    res.status(200).json(mockData);
+    let userExample = UserDAO.create({first_name: 'Yahor', last_name: 'Kutz'})
+    res.status(200).json(userExample);
 });
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,76 @@
-const webpack = require('webpack');
-const path = require('path');
-const fs = require('fs');
+// const webpack = require('webpack');
+// const path = require('path');
+// const fs = require('fs');
+//
+// let nodeModules = {};
+// fs.readdirSync('node_modules')
+//     .filter(function (x) {
+//         return ['.bin'].indexOf(x) === -1;
+//     })
+//     .forEach(function (mod) {
+//         nodeModules[mod] = 'commonjs ' + mod;
+//     });
+//
+// module.exports = {
+//     entry: './src/index.ts',
+//     target: 'node',
+//     output: {
+//         filename: 'index.js',
+//         path: path.resolve(__dirname, 'dist')
+//     },
+//     devtool: 'source-map',
+//     resolve: {
+//         // Add `.ts` and `.tsx` as a resolvable extension.
+//         extensions: [".ts", ".tsx", ".js"]
+//     },
+//     module: {
+//         rules: [
+//             { test: /\.tsx?$/, loader: "ts-loader" }
+//         ]
+//     },
+//     mode: "development"
+// };
+const path = require('path'), net = require('net'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-let nodeModules = {};
-fs.readdirSync('node_modules')
-    .filter(function (x) {
-        return ['.bin'].indexOf(x) === -1;
-    })
-    .forEach(function (mod) {
-        nodeModules[mod] = 'commonjs ' + mod;
-    });
+const sourcePath = path.join(__dirname, './src'),
+    buildPath = path.join(__dirname, process.env.NODE_ENV === 'test' ? './dist-testing' : './dist');
 
-module.exports = {
-    entry: './src/index.ts',
-    target: 'node',
+const conf = {
+    mode: "development",
+    entry: {
+        main: "./src/index.ts",
+    },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
+        path: buildPath
     },
-    devtool: 'source-map',
+    target: 'node',
+    plugins: [],
+    devtool: !(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') ? 'source-map' : false,
     resolve: {
-        // Add `.ts` and `.tsx` as a resolvable extension.
-        extensions: ['.ts', '.tsx', '.js']
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"],
+        alias: {'@src': sourcePath}
     },
     module: {
         rules: [
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-            { test: /\.tsx?$/, loader: 'ts-loader' }
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            {test: /\.tsx?$/, loader: "awesome-typescript-loader", exclude: /node_modules/},
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {enforce: "pre", test: /\.js$/, loader: "source-map-loader", exclude: /node_modules/},
         ]
     },
-    externals: nodeModules
+    devServer: {
+        contentBase: path.join(__dirname, process.env.NODE_ENV === 'test' ? "dist-testing" : "dist"),
+        compress: true,
+        disableHostCheck: true,
+        host: '0.0.0.0',
+        port: 9000,
+        hot: true,
+        https: true,
+    },
 };
+
+module.exports = conf;
